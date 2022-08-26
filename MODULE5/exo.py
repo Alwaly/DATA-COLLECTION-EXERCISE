debd557e3d5f21043e45380479905807427c46d7
AlwalyLengo <alwaly@lengo.africa>
2022-08-26 16:25:46 +0000

    Fix some issues

--- MODULE5/exo.py
+++ MODULE5/exo.py
@@ -7,30 +7,38 @@ import pandas as pd
 from libraries.countries import ApiGetter
 from libraries.db import DataBase
 from typing import Union
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return Utils.organizeData()
 if __name__ == '__main__':
-    data=Utils.concatData(HtmlFactory.main(),CsvFactory.main(),JsonFactory.main())
+    htmlData = HtmlFactory.main()
+
+    csvData = CsvFactory.main()
+
+    jsonData = JsonFactory.main()
+    data=Utils.concatData(htmlData,csvData,jsonData)
+
     df=pd.DataFrame(data)
+
     devisesDF=pd.DataFrame(CurrencyScrapper.makeCurrencyList())
+
     devisesTab=["Euro", "Dollar us", "Yen japonais"]
+
     df['Devise'] = ""
+
     df["XOF Value"]=0
+
     df['Devise'] = df['Devise'] \
         .apply(lambda x: Utils.randomise(devisesTab))
+
     df=Utils.convertDevise(df, devisesDF)
+
     countriesInformations=ApiGetter.getNameAndFlag()
+
     countriesDf=pd.DataFrame(countriesInformations)
+
     df['Country'] = ""
+
     df['Flag'] = ""
+
     Utils.randomiseCountryInfo(countriesInformations,df)
-    print(Utils.divider())
     #DataBase.create('DATACOLLECTIONEXERCISE')
     #DataBase.CreateCountryTable("DATACOLLECTIONEXERCISE")
     #DataBase.CreateDevisesTable("DATACOLLECTIONEXERCISE")
@@ -39,4 +47,4 @@ if __name__ == '__main__':
     #DataBase.insertDevises(devisesDF,"DATACOLLECTIONEXERCISE")
     #DataBase.insertCustommers(df,"DATACOLLECTIONEXERCISE")
     
-    
+    print(Utils.organizeData())
